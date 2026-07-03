e5ee25e7d32f578a8bf8534116fea68c4eb3bf23
jhon-henao13 <jhonestiwarhenao@gmail.com>
2026-07-02 21:03:36 -0500

    v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Aquí añadirás colores corporativos de 'wepromeurope' más adelante
+    },
+  },
+  plugins: [],
+}
